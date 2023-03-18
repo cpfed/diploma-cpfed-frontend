d9ed7a746f6a28f0a1056b735bd072caa7187e2c
ergossteam <ergossteam@gmail.com>
2023-03-19 02:41:43 +0600

    fix: hide faq elements

--- src/components/pages/index/FAQ/index.tsx
+++ src/components/pages/index/FAQ/index.tsx
@@ -10,12 +10,12 @@ export const elements: FAQElement[] = [
 	{
 		question: 'Как будут проходить отборочные туры?',
         answer: "Lorem pisum dolor sit amet, consectetur adpisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscpit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        isHidden: false,
+        isHidden: true,
 	},
 	{
 		question: 'По какой системе будут начисляться баллы?',
         answer: "Lorem pisum dolor sit amet, consectetur adpisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscpit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        isHidden: false,
+        isHidden: true,
 	},
 	{
 		question: 'Кто может принять участие?',
