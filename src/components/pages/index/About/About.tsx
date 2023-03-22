0fa129042ebcc49e1692bc5d95c5ffadfb62a37e
kzhamauov <KZhamauov@one.kz>
2023-03-23 02:53:55 +0600

    feat: added fund info in about section

--- src/components/pages/index/About/About.tsx
+++ src/components/pages/index/About/About.tsx
@@ -4,13 +4,31 @@ import Link from "next/link";
 import Container from "@/components/ui/Container";
 
 import classes from "./About.module.scss";
+import useTranslation from "next-translate/useTranslation";
+
+import { elements } from ".";
 
 const About = () => {
+    const { t } = useTranslation();
     return <section className={classes.about} id="about">
             <Container>
                 <div className={classes.about__content}>
-                    <p className={classes.about__content_title}>О чемпионате</p>
-                    <p className={classes.about__content_subtitle}>Соревнование для школьников, студентов и профессионалов по решению алгоритмических задач. Чемпионат будет проходит в формате, приближенном к международным студенческим соревнованиям по спортивному программированию. По результатам отборочных этапов лучшие 30 участников будут приглашены на финальный этап, в котором определятся лучшие!</p>
+                    <p className={classes.about__content_title}>{t('about:title')}</p>
+                    <p className={classes.about__content_subtitle}>{t('about:subtitle')}</p>
+                    <Link href="/championship" className={classes.about__more}>
+                        {t('about:more')}
+                    </Link>
+                </div>
+
+                <div className={classes.about__fund}>
+                    <p className={classes.about__fund_title}>{t('about:fund-title')}</p>
+                    <ul className={classes.about__fund_places}>
+                    {elements.map((element, index, self) => {
+                        return (
+                            <li className={classes.about__fund_places_place}>{t(element.description)}</li>
+                        )
+                    })}
+                    </ul>
                 </div>
             </Container>
         </section>
--- src/components/pages/index/About/index.tsx
+++ src/components/pages/index/About/index.tsx
@@ -1,1 +1,17 @@
+interface Place {
+	description: string
+}
+
+export const elements: Place[] = [
+	{
+        description: "about:fund-first-place",
+	},
+    {
+        description: "about:fund-second-place",
+	},
+    {
+        description: "about:fund-third-place",
+	}
+]
+
 export { default } from './About'
