c0d53a20d903246d1fd627a08cbd10ccfd2d051e
ergossteam <ergossteam@gmail.com>
2023-03-23 20:03:21 +0600

    feat: add CPFED_API_URL to nextconfig

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextTranslate = require('next-translate');
 const nextConfig = {
   reactStrictMode: true,
   ...nextTranslate(),
+  env: {
+    CPFED_API_URL: process.env.CPFED_API_URL
+  }
 };
 
 module.exports = nextConfig;
