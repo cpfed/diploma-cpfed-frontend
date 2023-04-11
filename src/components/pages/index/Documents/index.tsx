a54b4727151f83c8d4603fdcd75ffb74c59daee0
kzhamauov <KZhamauov@one.kz>
2023-04-11 15:14:45 +0600

    feat: added real links to usefull documents

--- src/components/pages/index/Documents/index.tsx
+++ src/components/pages/index/Documents/index.tsx
@@ -3,15 +3,14 @@ interface DocumentLink {
     link: string
 }
 
-// TODO: redirect to usefull links
 export const elements: DocumentLink[] = [
     {
         title: "documents:document-title-1",
-        link: "localhost:3000/fake",
+        link: "documents:document-link-1",
     },
     {
         title: "documents:document-title-2",
-        link: "localhost:3000/fake",
+        link: "documents:document-link-2",
     },
 ]
 export { default } from './Documents'
