78ce2dcf15eea99c753eb4746875183de125f8b0
kzhamauov <KZhamauov@one.kz>
2023-04-11 18:39:53 +0600

    fix: added links to FAQ

--- src/components/pages/index/FAQ/index.tsx
+++ src/components/pages/index/FAQ/index.tsx
@@ -6,14 +6,27 @@ interface FAQElement {
 	question: string
     answer: string
     isHidden: boolean
+	withLink: boolean
+	link: string
 }
 
 const maxFaq = 7;
 
 export const elements: FAQElement[] = range(maxFaq).map(value=>{
+	if (value == maxFaq) {
+		return {
+			question: `faq:faq-${value}`,
+			answer: `faq:faq-${value}-content`,
+			isHidden: true,
+			withLink: true,
+			link: "https://t.me/cpfed"
+		}
+	} 
 	return {
 		question: `faq:faq-${value}`,
 		answer: `faq:faq-${value}-content`,
-		isHidden: true
+		isHidden: true,
+		withLink: false,
+		link: ""
 	}
 });
