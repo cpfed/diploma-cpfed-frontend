ed78c6c57d3c4f460e47d1f64779ec1e79e78f56
oshortanbaiuly <olzhas.shortanbaiuly@nu.edu.kz>
2026-04-30 11:29:39 +0500

    fix: change link for olympiad regulations

--- next.config.js
+++ next.config.js
@@ -5,10 +5,9 @@ const nextTranslate = require('next-translate');
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-  ...nextTranslate(),
   env: {
     CPFED_API_URL: process.env.CPFED_API_URL
   }
 };
 
-module.exports = nextConfig;
+module.exports = nextTranslate(nextConfig);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
+import appWithI18n from "next-translate/appWithI18n";
+import i18nConfig from "../../i18n.json";
 import Layout from "@/components/layouts/Layout";
 import Profile from "@/components/layouts/Profile";
 import { useRouter } from "next/router";
@@ -9,7 +11,13 @@ import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
 import store from "@/store";
 
-export default function App({ Component, pageProps }: AppProps) {
+const i18nConfigWithLoader = {
+    ...i18nConfig,
+    loadLocaleFrom: (lang: string, ns: string) =>
+        import(`../../locales/${lang}/${ns}.json`).then((m) => m.default),
+};
+
+function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
 
     return (
@@ -38,3 +46,5 @@ export default function App({ Component, pageProps }: AppProps) {
         </Provider>
     );
 }
+
+export default appWithI18n(App as any, i18nConfigWithLoader);
